46a289a8c10ea1a925dc36c6358eb23338818ce1
Szary <dzimel94@gmail.com>
2015-08-02 14:29:52 +0200

    Create virtual class - option to menu

---
+++ Menu/Menu/Option.h
@@ -0,0 +1,11 @@
+#include <string>
+
+class Option {
+public:
+	Option(const std::string& name):_name(name) {}
+
+	const std::string& name() { return _name; }
+	bool virtual run() = 0;
+private:
+	std::string _name;
+};
